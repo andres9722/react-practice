2c5433adfec6ac985663c864ab189a8f551e7eae
andres9722 <andres_9722@outlook.com>
2018-11-26 16:56:27 -0500

    Updates

---
+++ precache-manifest.10383c3c98207d6c65feb9ecceeb6007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86636209bb23d9c32870e0c4c4d5d9ba",
+    "url": "/tuiter/static/media/loginbg.86636209.jpg"
+  },
+  {
+    "revision": "f1c43afcac8d2164db1d",
+    "url": "/tuiter/static/js/runtime~main.f1c43afc.js"
+  },
+  {
+    "revision": "69a558abd6707030e198",
+    "url": "/tuiter/static/js/main.69a558ab.chunk.js"
+  },
+  {
+    "revision": "55d055f7873ab8d3853c",
+    "url": "/tuiter/static/js/1.55d055f7.chunk.js"
+  },
+  {
+    "revision": "69a558abd6707030e198",
+    "url": "/tuiter/static/css/main.0af4129a.chunk.css"
+  },
+  {
+    "revision": "55d055f7873ab8d3853c",
+    "url": "/tuiter/static/css/1.1577fe3d.chunk.css"
+  },
+  {
+    "revision": "05ea3784906ad87b377acb2ee7fd5b84",
+    "url": "/tuiter/index.html"
+  }
+];
